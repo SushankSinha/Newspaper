bf45a43c2bf162b5ac96e0953ad6c752fb29f1e3
SushankSinha <SushankSinha>
2023-04-13 13:30:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let image1 = document.getElementById('img1');
+image1.addEventListener("contextmenu", function (e){
+    e.preventDefault();
+});
+let image2 = document.getElementById('img2');
+image2.addEventListener("contextmenu", function (e){
+    e.preventDefault();
+});
+let image3 = document.getElementById('img3');
+image3.addEventListener("contextmenu", function (e){
+    e.preventDefault();
+});
+let image4 = document.getElementById('img4');
+image4.addEventListener("contextmenu", function (e){
+    e.preventDefault();
+});
+let image5 = document.getElementById('img5');
+image5.addEventListener("contextmenu", function (e){
+    e.preventDefault();
+});
